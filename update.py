9c90ec1566f0d683f9031cf0cb8d01e4604cdbb7
blayzenw <blayzenw@gmail.com>
2013-11-30 11:01:03 -0800

    updating update

--- update.py
+++ update.py
@@ -1,8 +1,17 @@
 import urllib2
 
-versionURL = 
+versionURL = https://raw.github.com/blayzenw/Server_Watch/master/server%20watch.info
 
 def getCurrentVersion():
     page = urllib2.urlopen(versionURL)
     pageSource = page.read()
-    
+    newVersion = parseVersion(pageSource)
+
+    versionFile = open('server watch.info')
+    cv = versionFile.read()
+    currentVersion = parseVersion(cv)
+
+    if(newVersion != currentVersion)
+
+def parseVersion(text):
+    return int(text.split('=')[1])
