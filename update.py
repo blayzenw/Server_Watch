bdf61ad57ae2570b02ffd3908bab0088742134c7
blayzenw <blayzenw@gmail.com>
2013-11-30 11:33:45 -0800

    added update

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 import urllib2
+import zipfile
 
-versionURL = https://raw.github.com/blayzenw/Server_Watch/master/server%20watch.info
+versionURL = 'https://raw.github.com/blayzenw/Server_Watch/master/server%20watch.info'
+paylaodURL = 'https://github.com/blayzenw/Server_Watch/raw/master/Server%20Watch.zip'
 
 def getCurrentVersion():
     page = urllib2.urlopen(versionURL)
@@ -11,7 +13,22 @@ def getCurrentVersion():
     cv = versionFile.read()
     currentVersion = parseVersion(cv)
 
-    if(newVersion != currentVersion)
+    if(newVersion != currentVersion):
+        downloadNewVersion()
+        extractPayload()
 
 def parseVersion(text):
     return int(text.split('=')[1])
+
+def downloadNewVersion():
+    page = urllib2.urlopen(paylaodURL)
+    sw = open('Server Watch.zip', 'wb')
+    sw.write(page.read())
+    sw.close()
+
+def extractPayload():
+    swZip = open('Server Watch.zip', 'rb')
+    z = zipfile.ZipFile(swZip)
+    z.extractall()
+    swZip.close()
+    
